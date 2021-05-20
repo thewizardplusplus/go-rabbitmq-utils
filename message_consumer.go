61c2d3ba29cd788c8b667107a6318e069b5b1d91
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-20 23:00:00 +0300

    Add the rabbitmqutils.MessageConsumer structure

--- message_consumer.go
+++ message_consumer.go
@@ -1,10 +1,22 @@
 package rabbitmqutils
 
 import (
+	"context"
+
 	"github.com/streadway/amqp"
 )
 
 // MessageHandler ...
 type MessageHandler interface {
 	HandleMessage(message amqp.Delivery)
+}
+
+// MessageConsumer ...
+type MessageConsumer struct {
+	client               Client
+	queue                string
+	messages             <-chan amqp.Delivery
+	messageHandler       MessageHandler
+	stoppingCtx          context.Context
+	stoppingCtxCanceller context.CancelFunc
 }
