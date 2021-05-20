ba46a37f052eca5b2aa0beeb8e712389b378b660
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-20 23:00:00 +0300

    Add the rabbitmqutils.NewClient() function

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package rabbitmqutils
 
 import (
+	"github.com/pkg/errors"
 	"github.com/streadway/amqp"
 )
 
@@ -8,4 +9,20 @@ import (
 type Client struct {
 	connection *amqp.Connection
 	channel    *amqp.Channel
+}
+
+// NewClient ...
+func NewClient(dsn string) (Client, error) {
+	connection, err := amqp.Dial(dsn)
+	if err != nil {
+		return Client{}, errors.Wrap(err, "unable to open the connection")
+	}
+
+	channel, err := connection.Channel()
+	if err != nil {
+		return Client{}, errors.Wrap(err, "unable to open the channel")
+	}
+
+	client := Client{connection: connection, channel: channel}
+	return client, nil
 }
