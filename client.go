cb4af965eb1775636c3e96d88595e8ff2e5a8b09
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-20 23:00:00 +0300

    Add the rabbitmqutils.Client.PublishMessage() method

--- client.go
+++ client.go
@@ -1,6 +1,8 @@
 package rabbitmqutils
 
 import (
+	"encoding/json"
+
 	"github.com/pkg/errors"
 	"github.com/streadway/amqp"
 )
@@ -25,6 +27,26 @@ func NewClient(dsn string) (Client, error) {
 
 	client := Client{connection: connection, channel: channel}
 	return client, nil
+}
+
+// PublishMessage ...
+func (client Client) PublishMessage(queue string, message interface{}) error {
+	messageAsJSON, err := json.Marshal(message)
+	if err != nil {
+		return errors.Wrap(err, "unable to marshal the message")
+	}
+
+	if err := client.channel.Publish(
+		"",    // exchange
+		queue, // queue name
+		false, // mandatory
+		false, // immediate
+		amqp.Publishing{ContentType: "application/json", Body: messageAsJSON},
+	); err != nil {
+		return errors.Wrap(err, "unable to publish the message")
+	}
+
+	return nil
 }
 
 // Close ...
