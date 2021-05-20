70a8ae7c9968015ff7298f5482af898a204bf818
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-20 23:00:00 +0300

    Add the rabbitmqutils.NewMessageConsumer() function

--- message_consumer.go
+++ message_consumer.go
@@ -3,6 +3,7 @@ package rabbitmqutils
 import (
 	"context"
 
+	"github.com/pkg/errors"
 	"github.com/streadway/amqp"
 )
 
@@ -19,4 +20,27 @@ type MessageConsumer struct {
 	messageHandler       MessageHandler
 	stoppingCtx          context.Context
 	stoppingCtxCanceller context.CancelFunc
+}
+
+// NewMessageConsumer ...
+func NewMessageConsumer(
+	client Client,
+	queue string,
+	messageHandler MessageHandler,
+) (MessageConsumer, error) {
+	messages, err := client.ConsumeMessages(queue)
+	if err != nil {
+		return MessageConsumer{}, errors.Wrap(err, "unable to start the consuming")
+	}
+
+	stoppingCtx, stoppingCtxCanceller := context.WithCancel(context.Background())
+	messageConsumer := MessageConsumer{
+		client:               client,
+		queue:                queue,
+		messages:             messages,
+		messageHandler:       messageHandler,
+		stoppingCtx:          stoppingCtx,
+		stoppingCtxCanceller: stoppingCtxCanceller,
+	}
+	return messageConsumer, nil
 }
