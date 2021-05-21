a782d16af3465356cb92eb9307cd36f67de301cf
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-21 23:00:00 +0300

    Implement the tests of the rabbitmqutils.NewMessageConsumer() function

--- message_consumer_test.go
+++ message_consumer_test.go
@@ -2,7 +2,9 @@ package rabbitmqutils
 
 import (
 	"testing"
+	"testing/iotest"
 
+	"github.com/streadway/amqp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -20,7 +22,60 @@ func TestNewMessageConsumer(test *testing.T) {
 		wantedMessageConsumer func(test *testing.T, messageConsumer MessageConsumer)
 		wantedErr             assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			args: args{
+				client: func() MessageConsumerClient {
+					client := new(MockMessageConsumerClient)
+					client.
+						On("ConsumeMessages", "test").
+						Return(make(<-chan amqp.Delivery), nil)
+
+					return client
+				}(),
+				queue:          "test",
+				messageHandler: new(MockMessageHandler),
+			},
+			wantedMessageConsumer: func(
+				test *testing.T,
+				messageConsumer MessageConsumer,
+			) {
+				assert.IsType(test, new(MockMessageConsumerClient), messageConsumer.client)
+				assert.Equal(test, "test", messageConsumer.queue)
+				assert.Len(test, messageConsumer.messages, 0)
+				assert.Equal(test, new(MockMessageHandler), messageConsumer.messageHandler)
+
+				for _, field := range []interface{}{
+					messageConsumer.client,
+					messageConsumer.messages,
+					messageConsumer.stoppingCtx,
+					messageConsumer.stoppingCtxCanceller,
+				} {
+					assert.NotNil(test, field)
+				}
+			},
+			wantedErr: assert.NoError,
+		},
+		{
+			name: "error",
+			args: args{
+				client: func() MessageConsumerClient {
+					client := new(MockMessageConsumerClient)
+					client.On("ConsumeMessages", "test").Return(nil, iotest.ErrTimeout)
+
+					return client
+				}(),
+				queue:          "test",
+				messageHandler: new(MockMessageHandler),
+			},
+			wantedMessageConsumer: func(
+				test *testing.T,
+				messageConsumer MessageConsumer,
+			) {
+				assert.Zero(test, messageConsumer)
+			},
+			wantedErr: assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			receivedMessageConsumer, receivedErr := NewMessageConsumer(
