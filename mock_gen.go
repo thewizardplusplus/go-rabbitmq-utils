49e135edcf126661a3308ccca2baf7db3a6e7fd4
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-27 23:00:00 +0300

    Generate the mock for the rabbitmqutils.Dialer type

--- mock_gen.go
+++ mock_gen.go
@@ -1,5 +1,15 @@
 package rabbitmqutils
 
+//go:generate mockery --name=DialerInterface --inpackage --case=underscore --testonly
+
+// DialerInterface ...
+//
+// It is used only for mock generating.
+//
+type DialerInterface interface {
+	Dial(dsn string) (MessageBrokerConnection, error)
+}
+
 //go:generate mockery --name=ContextCancellerInterface --inpackage --case=underscore --testonly
 
 // ContextCancellerInterface ...
