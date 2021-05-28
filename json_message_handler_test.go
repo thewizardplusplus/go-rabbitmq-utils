c25b53872934a38d10c07b95e22547872fa5c12c
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-28 23:00:00 +0300

    Implement the tests of the rabbitmqutils.JSONMessageHandler.HandleMessage() method

--- json_message_handler_test.go
+++ json_message_handler_test.go
@@ -1,7 +1,9 @@
 package rabbitmqutils
 
 import (
+	"reflect"
 	"testing"
+	"testing/iotest"
 
 	"github.com/streadway/amqp"
 	"github.com/stretchr/testify/assert"
@@ -25,7 +27,60 @@ func TestJSONMessageHandler_HandleMessage(test *testing.T) {
 		args      args
 		wantedErr assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			fields: fields{
+				MessageHandler: func() SpecificMessageHandler {
+					messageHandler := new(MockSpecificMessageHandler)
+					messageHandler.On("MessageType").Return(reflect.TypeOf(testMessage{}))
+					messageHandler.
+						On("HandleMessage", testMessage{FieldOne: 23, FieldTwo: "one"}).
+						Return(nil)
+
+					return messageHandler
+				}(),
+			},
+			args: args{
+				message: amqp.Delivery{Body: []byte(`{"FieldOne":23,"FieldTwo":"one"}`)},
+			},
+			wantedErr: assert.NoError,
+		},
+		{
+			name: "error with unmarshalling",
+			fields: fields{
+				MessageHandler: func() SpecificMessageHandler {
+					messageHandler := new(MockSpecificMessageHandler)
+					messageHandler.On("MessageType").Return(reflect.TypeOf(func() {}))
+					messageHandler.
+						On("HandleMessage", testMessage{FieldOne: 23, FieldTwo: "one"}).
+						Return(nil)
+
+					return messageHandler
+				}(),
+			},
+			args: args{
+				message: amqp.Delivery{Body: []byte(`{"FieldOne":23,"FieldTwo":"one"}`)},
+			},
+			wantedErr: assert.Error,
+		},
+		{
+			name: "error with handling",
+			fields: fields{
+				MessageHandler: func() SpecificMessageHandler {
+					messageHandler := new(MockSpecificMessageHandler)
+					messageHandler.On("MessageType").Return(reflect.TypeOf(testMessage{}))
+					messageHandler.
+						On("HandleMessage", testMessage{FieldOne: 23, FieldTwo: "one"}).
+						Return(iotest.ErrTimeout)
+
+					return messageHandler
+				}(),
+			},
+			args: args{
+				message: amqp.Delivery{Body: []byte(`{"FieldOne":23,"FieldTwo":"one"}`)},
+			},
+			wantedErr: assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			handler := JSONMessageHandler{
