7f59ed885b596821918a7cca02a2345a0ab9c2c2
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-28 23:00:00 +0300

    Add the rabbitmqutils.JSONMessageHandler structure

--- json_message_handler.go
+++ json_message_handler.go
@@ -9,3 +9,8 @@ type SpecificMessageHandler interface {
 	MessageType() reflect.Type
 	HandleMessage(message interface{}) error
 }
+
+// JSONMessageHandler ...
+type JSONMessageHandler struct {
+	MessageHandler SpecificMessageHandler
+}
