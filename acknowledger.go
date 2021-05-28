cf800e6fa63c0776c524a6dbe1d6873ea239f48c
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-28 23:00:00 +0300

    Add the rabbitmqutils.Acknowledger.HandleMessage() method

--- acknowledger.go
+++ acknowledger.go
@@ -25,3 +25,19 @@ type Acknowledger struct {
 	MessageHandler  FailingMessageHandler
 	Logger          log.Logger
 }
+
+// HandleMessage ...
+func (acknowledger Acknowledger) HandleMessage(message amqp.Delivery) {
+	if err := acknowledger.MessageHandler.HandleMessage(message); err != nil {
+		acknowledger.Logger.Logf("unable to handle the message: %v", err)
+
+		requeue :=
+			acknowledger.MessageHandling == TwiceMessageHandling && !message.Redelivered
+		message.Reject(requeue)
+
+		return
+	}
+
+	acknowledger.Logger.Log("message has been handled successfully")
+	message.Ack(false /* multiple */)
+}
