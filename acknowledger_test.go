a32209ef4b921605b64aa087a3d9dcbd1f543a67
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-28 23:00:00 +0300

    Implement the tests of the rabbitmqutils.Acknowledger.HandleMessage() method

--- acknowledger_test.go
+++ acknowledger_test.go
@@ -1,7 +1,9 @@
 package rabbitmqutils
 
 import (
+	"reflect"
 	"testing"
+	"testing/iotest"
 
 	"github.com/go-log/log"
 	"github.com/streadway/amqp"
@@ -23,7 +25,127 @@ func TestAcknowledger_HandleMessage(test *testing.T) {
 		fields fields
 		args   args
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			fields: fields{
+				MessageHandling: OnceMessageHandling,
+				MessageHandler: func() FailingMessageHandler {
+					messageHandler := new(MockFailingMessageHandler)
+					messageHandler.
+						On("HandleMessage", mock.MatchedBy(func(message amqp.Delivery) bool {
+							message.Acknowledger = nil
+
+							return reflect.DeepEqual(message, amqp.Delivery{
+								DeliveryTag: 23,
+								Body:        []byte("test"),
+							})
+						})).
+						Return(nil)
+
+					return messageHandler
+				}(),
+				Logger: func() log.Logger {
+					logger := new(MockLogger)
+					logger.On("Log", "message has been handled successfully").Return()
+
+					return logger
+				}(),
+			},
+			args: args{
+				message: amqp.Delivery{
+					Acknowledger: func() amqp.Acknowledger {
+						amqpAcknowledger := new(MockAMQPAcknowledger)
+						amqpAcknowledger.On("Ack", uint64(23), false).Return(nil)
+
+						return amqpAcknowledger
+					}(),
+					DeliveryTag: 23,
+					Body:        []byte("test"),
+				},
+			},
+		},
+		{
+			name: "error with once message handling",
+			fields: fields{
+				MessageHandling: OnceMessageHandling,
+				MessageHandler: func() FailingMessageHandler {
+					messageHandler := new(MockFailingMessageHandler)
+					messageHandler.
+						On("HandleMessage", mock.MatchedBy(func(message amqp.Delivery) bool {
+							message.Acknowledger = nil
+
+							return reflect.DeepEqual(message, amqp.Delivery{
+								DeliveryTag: 23,
+								Body:        []byte("test"),
+							})
+						})).
+						Return(iotest.ErrTimeout)
+
+					return messageHandler
+				}(),
+				Logger: func() log.Logger {
+					logger := new(MockLogger)
+					logger.
+						On("Logf", "unable to handle the message: %v", iotest.ErrTimeout).
+						Return()
+
+					return logger
+				}(),
+			},
+			args: args{
+				message: amqp.Delivery{
+					Acknowledger: func() amqp.Acknowledger {
+						amqpAcknowledger := new(MockAMQPAcknowledger)
+						amqpAcknowledger.On("Reject", uint64(23), false).Return(nil)
+
+						return amqpAcknowledger
+					}(),
+					DeliveryTag: 23,
+					Body:        []byte("test"),
+				},
+			},
+		},
+		{
+			name: "error with twice message handling",
+			fields: fields{
+				MessageHandling: TwiceMessageHandling,
+				MessageHandler: func() FailingMessageHandler {
+					messageHandler := new(MockFailingMessageHandler)
+					messageHandler.
+						On("HandleMessage", mock.MatchedBy(func(message amqp.Delivery) bool {
+							message.Acknowledger = nil
+
+							return reflect.DeepEqual(message, amqp.Delivery{
+								DeliveryTag: 23,
+								Body:        []byte("test"),
+							})
+						})).
+						Return(iotest.ErrTimeout)
+
+					return messageHandler
+				}(),
+				Logger: func() log.Logger {
+					logger := new(MockLogger)
+					logger.
+						On("Logf", "unable to handle the message: %v", iotest.ErrTimeout).
+						Return()
+
+					return logger
+				}(),
+			},
+			args: args{
+				message: amqp.Delivery{
+					Acknowledger: func() amqp.Acknowledger {
+						amqpAcknowledger := new(MockAMQPAcknowledger)
+						amqpAcknowledger.On("Reject", uint64(23), true).Return(nil)
+
+						return amqpAcknowledger
+					}(),
+					DeliveryTag: 23,
+					Body:        []byte("test"),
+				},
+			},
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			acknowledger := Acknowledger{
