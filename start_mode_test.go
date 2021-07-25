1bd9fd83c00b49d473663b7e6332e4716e92303e
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-07-25 23:00:00 +0300

    Implement the tests of the rabbitmqutils.startModeHolder.setStartModeOnce() method

--- start_mode_test.go
+++ start_mode_test.go
@@ -27,7 +27,26 @@ func Test_startModeHolder_setStartModeOnce(test *testing.T) {
 		args            args
 		wantedStartMode startMode
 	}{
-		// TODO: Add test cases.
+		{
+			name: "with the default start mode",
+			fields: fields{
+				mode: notStarted,
+			},
+			args: args{
+				mode: 42,
+			},
+			wantedStartMode: 42,
+		},
+		{
+			name: "with the not default start mode",
+			fields: fields{
+				mode: 23,
+			},
+			args: args{
+				mode: 42,
+			},
+			wantedStartMode: 23,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			holder := &startModeHolder{
