ffded48268cfe3ff661fb626630183495a728d69
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-11-10 23:00:00 +0300

    Update the rabbitmqutils.MessageConsumer structure

--- message_consumer.go
+++ message_consumer.go
@@ -2,10 +2,10 @@ package rabbitmqutils
 
 import (
 	"context"
-	"sync"
 
 	"github.com/pkg/errors"
 	"github.com/streadway/amqp"
+	syncutils "github.com/thewizardplusplus/go-sync-utils"
 )
 
 //go:generate mockery --name=MessageConsumerClient --inpackage --case=underscore --testonly
@@ -23,15 +23,24 @@ type MessageHandler interface {
 	HandleMessage(message amqp.Delivery)
 }
 
+type messageHandlerWrapper struct {
+	messageHandler MessageHandler
+}
+
+func (wrapper messageHandlerWrapper) Handle(
+	ctx context.Context,
+	data interface{},
+) {
+	wrapper.messageHandler.HandleMessage(data.(amqp.Delivery))
+}
+
 // MessageConsumer ...
 type MessageConsumer struct {
-	client               MessageConsumerClient
-	queue                string
-	messages             <-chan amqp.Delivery
-	messageHandler       MessageHandler
-	startMode            *startModeHolder
-	stoppingCtx          context.Context
-	stoppingCtxCanceller context.CancelFunc
+	// do not use embedding to hide the Handle() method
+	concurrentHandler syncutils.ConcurrentHandler
+	client            MessageConsumerClient
+	queue             string
+	stoppingCtx       context.Context
 }
 
 // NewMessageConsumer ...
@@ -45,45 +54,36 @@ func NewMessageConsumer(
 		return MessageConsumer{}, errors.Wrap(err, "unable to start the consuming")
 	}
 
-	startMode := &startModeHolder{}
+	concurrentHandler := syncutils.NewConcurrentHandler(0, messageHandlerWrapper{
+		messageHandler: messageHandler,
+	})
 	stoppingCtx, stoppingCtxCanceller := context.WithCancel(context.Background())
+	go func() {
+		defer stoppingCtxCanceller()
+
+		for message := range messages {
+			concurrentHandler.Handle(message)
+		}
+	}()
+
 	messageConsumer := MessageConsumer{
-		client:               client,
-		queue:                queue,
-		messages:             messages,
-		messageHandler:       messageHandler,
-		startMode:            startMode,
-		stoppingCtx:          stoppingCtx,
-		stoppingCtxCanceller: stoppingCtxCanceller,
+		concurrentHandler: concurrentHandler,
+		client:            client,
+		queue:             queue,
+		stoppingCtx:       stoppingCtx,
 	}
 	return messageConsumer, nil
 }
 
 // Start ...
 func (consumer MessageConsumer) Start() {
-	consumer.basicStart(started, func() {
-		for message := range consumer.messages {
-			consumer.messageHandler.HandleMessage(message)
-		}
-	})
+	consumer.concurrentHandler.Start(context.Background())
 }
 
 // StartConcurrently ...
-func (consumer MessageConsumer) StartConcurrently(concurrency int) {
-	consumer.basicStart(startedConcurrently, func() {
-		var waitGroup sync.WaitGroup
-		waitGroup.Add(concurrency)
-
-		for i := 0; i < concurrency; i++ {
-			go func() {
-				defer waitGroup.Done()
-
-				consumer.Start()
-			}()
-		}
-
-		waitGroup.Wait()
-	})
+func (consumer MessageConsumer) StartConcurrently(concurrencyFactor int) {
+	consumer.concurrentHandler.
+		StartConcurrently(context.Background(), concurrencyFactor)
 }
 
 // Stop ...
@@ -91,17 +91,8 @@ func (consumer MessageConsumer) Stop() error {
 	if err := consumer.client.CancelConsuming(consumer.queue); err != nil {
 		return errors.Wrap(err, "unable to cancel the consuming")
 	}
-
 	<-consumer.stoppingCtx.Done()
+
+	consumer.concurrentHandler.Stop()
 	return nil
-}
-
-func (consumer MessageConsumer) basicStart(mode startMode, handler func()) {
-	consumer.startMode.setStartModeOnce(mode)
-
-	handler()
-
-	if consumer.startMode.getStartMode() == mode {
-		consumer.stoppingCtxCanceller()
-	}
 }
